4b7120bd3c7901bf7b983f8349f09c7115ee7123
Biditchoun <sawijnbe@shi-r4-p7.s19.be>
2026-02-18 22:28:20 +0100

    first "working" version (still has some bugs)

--- albc_brute_chunks.c
+++ albc_brute_chunks.c
@@ -6,33 +6,66 @@
 /*   By: sawijnbe <sawijnbe@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 18:37:14 by sawijnbe          #+#    #+#             */
-/*   Updated: 2026/02/17 09:16:32 by sawijnbe         ###   ########.fr       */
+/*   Updated: 2026/02/18 22:27:59 by sawijnbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	*determine_rotation(t_bc *par, int amount_in_a, int b_size)
+void	*determine_rotation(t_stack *b, int b_size)
 {
+	t_stack	*b2;
+	int		i;
+	int		j;
+	int		gap[2];
+
+	b2 = b->prev;
+	i = 0;
+	j = 1;
+	gap[1] = -1;
+	while (b != b2)
+	{
+		if (b->nb >= b_size - 5)
+			gap[0] = i;
+		if (b2->nb >= b_size - 5)
+			gap[1] = b_size - j;
+		b = b->next;
+		if (++i && b != b2 && ++j)
+			b2 = b2->prev;
+	}
+	if (gap[1] != -1 && gap[1] - gap[0] >= b_size - gap[1])
+		return (&rrb);
+	return (&rb);
 }
 
-bool	check_if_all_at_top(t_stack *b, int amount_in_a, int b_size)
+int	check_if_all_at_top(t_stack *b, int amount_in_a, int b_size)
 {
+	int	amount_in_b;
+
+	amount_in_b = 5 - amount_in_a;
+	while (amount_in_b--)
+	{
+		if (b->nb < b_size - 5)
+			return (0);
+		b = b->next;
+	}
+	return (1);
 }
 
 int	get_next_five_to_top(t_bc *par, int i, int *amount_in_a, int *b_size)
 {
 	int	(*rb_or_rrb)(t_stack **a, t_stack **b);
 
-	ra_or_rra = &ra;
+	rb_or_rrb = &rb;
 	while (rb_or_rrb)
 	{
-		rb_or_rrb = determine_rotation(par, *amount_in_a, *b_size);
+		if (check_if_all_at_top((*par->b), *amount_in_a, *b_size))
+			return (i);
+		while ((*par->b)->nb >= *b_size - 5 && ++(*amount_in_a))
+			par->instructs[i++] = pa(par->a, par->b);
+		rb_or_rrb = determine_rotation((*par->b), *b_size);
 		while ((*par->b)->nb < *b_size - 5)
 			par->instructs[i++] = rb_or_rrb(par->a, par->b);
-		if (!check_if_all_at_top((*par->b), *amount_in_a, *b_size))
-			while ((*par->b) >= *b_size - 5 && ++(*amount_in_a))
-				par->instructs[i++] = pa(par->a, par->b);
 	}
 	return (i);
 }
@@ -42,7 +75,7 @@ int	push_all_to_a(t_bc *par, int i, int b_size)
 	int	amount_in_a;
 	int	*bruteforce_rt;
 
-	while (*par->b)
+	while (b_size > 5)
 	{
 		amount_in_a = 0;
 		i = get_next_five_to_top(par, i, &amount_in_a, &b_size);
@@ -52,8 +85,14 @@ int	push_all_to_a(t_bc *par, int i, int b_size)
 		i += arr_cpy(&par->instructs[i], bruteforce_rt, INT_MAX, -1);
 		apply_instructs(par->a, par->b, bruteforce_rt, par->f_instructs);
 		free(bruteforce_rt);
-		b_size = b_size - 5 + amount_in_a;
+		b_size -= 5;
 	}
+	bruteforce_rt = bruteforce(par->a, par->b, amount_in_a, INT_MAX - 1);
+	if (!bruteforce_rt)
+		return (0);
+	i += arr_cpy(&par->instructs[i], bruteforce_rt, INT_MAX, -1);
+	apply_instructs(par->a, par->b, bruteforce_rt, par->f_instructs);
+	free(bruteforce_rt);
 	return (i);
 }
 
--- m_main.c
+++ m_main.c
@@ -6,13 +6,13 @@
 /*   By: sawijnbe <sawijnbe@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 11:47:52 by sawijnbe          #+#    #+#             */
-/*   Updated: 2026/02/12 22:33:15 by sawijnbe         ###   ########.fr       */
+/*   Updated: 2026/02/18 22:04:35 by sawijnbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	check_list(t_stack *a)
+/*void	check_list(t_stack *a)
 {
 #include <stdio.h>
 	t_stack *cp, *cp2;
@@ -34,7 +34,7 @@ void	check_list(t_stack *a)
 	}
 	printf("\n");
 }
-
+*/
 void	bubble_sort(int *a, int a_size)
 {
 	int	i;
